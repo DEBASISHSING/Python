4485f455f748d6edc6e4a98875502164b13c13f8
DEBASISHSING <debasishsing2020@gmail.com>
2025-04-04 21:18:46 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,22 @@
+# ASSIGNMENT 1
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the first number: "))
+
+# Addition
+
+c = a + b
+print("Addition: ",c)
+
+#Subtraction
+c = a - b
+print("Subtraction: ",c)
+
+#Multiplication
+c = a * b
+print("Multiplication: ",c)
+
+#Division
+c = a / b
+print("Division: ",c)
+
---
+++ Task2.py
@@ -0,0 +1,10 @@
+
+# Task
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+full_name = first_name + " " + last_name
+
+
+print(f"Hello,\n {full_name}! \n  Welcome to the Python program.")
